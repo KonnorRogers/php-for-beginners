1515a3161e961f667baa14b400911d289078754e
paramagicdev <konnor5456@gmail.com>
2019-08-18 21:18:46 -0400

    working on ch7

--- ch06/index.view.php
+++ ch06/index.view.php
@@ -2,13 +2,6 @@
 <html lang="en">
   <meta charset="UTF-8">
   <title>Document</title>
-  <style>
-    header {
-      background: #e3e3e3;
-      padding: 2rem;
-      text-align: center;
-    ***REMOVED***
-  </style>
   </head>
   <body>
     <ul>
