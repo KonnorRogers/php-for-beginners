2d1a2c8bf9215d28550325f4d8097db2f894f9a6
paramagicdev <konnor5456@gmail.com>
2019-08-27 16:40:25 -0400

    working on ch12

--- ch12/index.view.php
+++ ch12/index.view.php
@@ -1,11 +1,16 @@
 <!doctype html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title></title>
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      ***REMOVED*** foreach ($tasks as $task) : ?>
+        <li><?= $task->getDescription(); ?></li>
+      ***REMOVED*** endforeach; ?>
+    </ul>
 
-</body>
+  </body>
 </html>
